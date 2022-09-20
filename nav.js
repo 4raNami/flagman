2ada128ef0baec3ab52f19c96be3fa4bb7cf6e8c
4raNami <20a6019@sit.shonan-it.ac.jp>
2022-09-21 00:06:46 +0900

    indexのinclude化

---
+++ nav.js
@@ -0,0 +1,9 @@
+var nav=document.querySelector("header");
+nav.insertAdjacentHTML('beforeend',`<nav>
+  <ol class="navi-list">
+    <li><a href="#">portfolio予定地</a></li>
+    <li>何かの予定地</li>
+    <li>何かの予定地２</li>
+    <li>何かの予定地３</li>
+  </ol>
+</nav>`);

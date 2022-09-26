988c6ceaba82cc5b3afbda513e4d596883c02e09
4raNami <20a6019@sit.shonan-it.ac.jp>
2022-09-26 23:10:40 +0900

    in the portfolio

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,9 @@
 var nav=document.querySelector("header");
 nav.insertAdjacentHTML('beforeend',`<nav>
   <ol class="navi-list">
-    <li><a href="#">portfolio予定地</a></li>
+    <li><a href="https://4ranami.github.io/portfolio/">portfolio予定地</a></li>
     <li>何かの予定地</li>
-    <li>何かの予定地２</li>
-    <li>何かの予定地３</li>
+    <li>何かの予定</li>
+    <li>何かの予定</li>
   </ol>
 </nav>`);

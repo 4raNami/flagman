e6718c8ebcf19545092dd062ec8f619f8fcc03a0
4ranami <20a6019@sit.shonan-it.ac.jp>
2023-09-09 01:36:35 +0900

    make a small change

--- js/svEditor.js
+++ js/svEditor.js
@@ -5,9 +5,14 @@ function create(){
   const foff = parseInt(document.getElementById("fOffset").value);
   const fbpm = parseInt(document.getElementById("fBpm").value);
   let out="";
+  let rowCount=-1;
   let div = (fbpm-sbpm)/(foff-soff);
   for(var i=0; i<=foff-soff; i++){
-    out += `${soff+i},${60000/(sbpm+div*i)},4,1,0,100,1,0\n`;
+    var tmp = Math.floor(div*i);
+    if(rowCount<tmp){
+      out += `${soff+i},${60000/(sbpm+div*i)},4,1,0,100,1,0\n`;
+      rowCount = tmp;
+    }
   }
   output.value = out;
 }
